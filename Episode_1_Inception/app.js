182d2c0455fa1ad7bae81f2c7c7299194ae3f451
Bharat Katakam <bharatsaikumar.katakam@gmail.com>
2024-02-22 06:19:31 +0530

    nested children with multiple elements

--- Episode_1_Inception/app.js
+++ Episode_1_Inception/app.js
@@ -2,12 +2,14 @@
  * <div id ="parent">
  *   <div id="child">
  *      <h1 id="heading"> I'm h1 tag</h1>
+ *      <h2 id="h2">I'm h2 tag<h2>
  *   </div>
  * </div>
+ * 
  */
 
 const parent = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child" }, 
-    React.createElement("h1", { id: "heading" }, "Im H1 tag")))
+    [React.createElement("h1", { id: "heading" }, "Im H1 tag"),React.createElement("h2", { id: "heading2" }, "Im H2 tag")]))
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
