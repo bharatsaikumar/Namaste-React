f7cdbb68ec5ac5e1478740785b11480df084e3b2
Bharat Katakam <bharatsaikumar.katakam@gmail.com>
2024-02-22 06:11:40 +0530

    nested html structure

--- Episode_1_Inception/app.js
+++ Episode_1_Inception/app.js
@@ -1,3 +1,13 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello World from React");
+/**
+ * <div id ="parent">
+ *   <div id="child">
+ *      <h1 id="heading"> I'm h1 tag</h1>
+ *   </div>
+ * </div>
+ */
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" }, 
+    React.createElement("h1", { id: "heading" }, "Im H1 tag")))
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
