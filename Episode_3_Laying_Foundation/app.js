3fe6673db9e1e8e5079efd5a9ac25a2645ce95bc
Bharat Katakam <bharatsaikumar.katakam@gmail.com>
2024-03-03 20:42:11 +0530

    execute JS code inside JSX

--- Episode_3_Laying_Foundation/app.js
+++ Episode_3_Laying_Foundation/app.js
@@ -4,13 +4,13 @@ import ReactDOM from "react-dom/client";
 const jsxHeading = <h1 className="head">Namaste React from JSX</h1>;
 
 //functional component
-const Title = () => (
-     <h1>This is Title JSX</h1>
+const Title = (
+  <h1>This is Title JSX</h1>
 )
 const HeadingComponent = () => {
   return (
     <div id="container">
-        <Title></Title>
+        {Title}
       <h1>Namaste React using Functional Component</h1>
     </div>
   );
